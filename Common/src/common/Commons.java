d933f71837bfe9513e7d405c43b56c97cfd04d5a
Burcak Otlu <burcakotlu@gmail.com>
2014-03-25 16:31:41 +0200

    inputdataprocess and removeoverlaps

--- Common/src/common/Commons.java
+++ Common/src/common/Commons.java
@@ -42,8 +42,8 @@ public class Commons {
 	public static final String INPUT_FILE_FORMAT_GFF3_1_BASED_COORDINATES_START_INCLUSIVE_END_INCLUSIVE = "INPUT_FILE_FORMAT_GFF3_1_BASED_COORDINATES_START_INCLUSIVE_END_INCLUSIVE";
 	public static final String INPUT_FILE_FORMAT_0_BASED_COORDINATES_START_INCLUSIVE_END_INCLUSIVE = "INPUT_FILE_FORMAT_0_BASED_COORDINATES_START_INCLUSIVE_END_INCLUSIVE";
 			
-	public static final String PROCESSED_INPUT_FILE = System.getProperty("file.separator")+ "Doktora" + System.getProperty("file.separator") +  "processinputdata" + System.getProperty("file.separator") +"Processed_Input_Data.txt";
-	public static final String REMOVED_OVERLAPS_INPUT_FILE = System.getProperty("file.separator")+ "Doktora" + System.getProperty("file.separator") +  "processinputdata" + System.getProperty("file.separator") +"Removed_Overlaps_Input_Data.txt";
+	public static final String PROCESSED_INPUT_FILE = "Doktora" + System.getProperty("file.separator") +  "processinputdata" + System.getProperty("file.separator") +"Processed_Input_Data.txt";
+	public static final String REMOVED_OVERLAPS_INPUT_FILE = "Doktora" + System.getProperty("file.separator") +  "processinputdata" + System.getProperty("file.separator") +"Removed_Overlaps_Input_Data.txt";
 	
 	//FOR TESTING PURPOSES 
 	public static final String TEST_INPUT_FILE_BED_FORMAT = "";;
