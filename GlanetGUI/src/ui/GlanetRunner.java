057e91409fd0b77287ae667e3e29b5cb5a3e4a8e
canfirtina <cfirtina@ug.bilkent.edu.tr>
2014-08-23 12:52:47 -0400

    read from file

--- GlanetGUI/src/ui/GlanetRunner.java
+++ GlanetGUI/src/ui/GlanetRunner.java
@@ -4,12 +4,6 @@ import empiricalpvalues.AnnotatePermutationsWithNumbersWithChoices;
 import empiricalpvalues.CollectionofPermutationsResults;
 import giveninputdata.InputDataProcess;
 import giveninputdata.InputDataRemoveOverlaps;
-
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collection;
-import java.util.List;
-
 import jaxbxjctool.GenerationofSequencesandMatricesforGivenIntervals;
 import rsat.RSATMatrixScanClient;
 import annotate.intervals.parametric.AnnotateGivenIntervalsWithNumbersWithChoices;
@@ -21,63 +15,44 @@ public class GlanetRunner extends Thread{
 	
 	private static String args[];
 	private static MainView mainView;
-	private static String cellLines[];
 
 	@Override
 	public void run(){
 		
-		/*--------------------------------
-		 * IMPORTANT
-		 * USE cellLines[] WHEREVER YOU WANT HERE. THE SELECTED CELL LINES
-		 * ARE PASSED HERE AS A ARRAY OF STRINGS. IF NO CELL LINE IS SELECTED
-		 * THEN THE SIZE OF cellLines[] IS ZERO. CHECK THIS BEFORE USING IT.
-		 * -------------------------------
-		 */
-		
 		try {
-			
-			Collection<String> all = new ArrayList<String>();
-			List<String> col1 = Arrays.asList(getArgs());
-			List<String> col2 = Arrays.asList(cellLines);
-			all.addAll(col1);
-			all.addAll(col2);
-			
-			//Combination getArgs and selected cellLines as String array
-			String[] allArguments = all.toArray(new String[] {});
-			
-		
+
 			getMainView().setCurrentProcessInfo( "InputDataProcess...");
-			InputDataProcess.main(allArguments);
+			InputDataProcess.main( args);
 			
 			/* In case of Enrichment remove overlaps and merge */
 			/* In case of only Annotation, do not remove overlaps and do not merge*/
 			if( getArgs()[4].equalsIgnoreCase(Commons.DO_ENRICH)){
 				getMainView().setCurrentProcessInfo( "RemoveOverlaps...");
-				InputDataRemoveOverlaps.main(allArguments);
+				InputDataRemoveOverlaps.main( args);
 			}
 							
 			getMainView().setCurrentProcessInfo( "Annotate Given Input Data...");
-			AnnotateGivenIntervalsWithNumbersWithChoices.main(allArguments);
+			AnnotateGivenIntervalsWithNumbersWithChoices.main( args);
 			
 			
 			if( getArgs()[4].equalsIgnoreCase(Commons.DO_ENRICH)){
 				
 				getMainView().setCurrentProcessInfo( "Annotate Permutations for Enrichment...");
-				AnnotatePermutationsWithNumbersWithChoices.main(allArguments);
+				AnnotatePermutationsWithNumbersWithChoices.main( args);
 				
 				getMainView().setCurrentProcessInfo( "Collection of Permutations Results...");
-				CollectionofPermutationsResults.main(allArguments);
+				CollectionofPermutationsResults.main( args);
 				
 				getMainView().setCurrentProcessInfo( "Augmentation of Enriched Elements with Given Input Data...");
-				AugmentationofEnrichedElementswithGivenInputData.main(allArguments);
+				AugmentationofEnrichedElementswithGivenInputData.main( args);
 				
 				
 				if( getArgs()[16].equalsIgnoreCase(Commons.DO_REGULATORY_SEQUENCE_ANALYSIS_USING_RSAT)) {
 					
 					getMainView().setCurrentProcessInfo( "GenerationofSequencesandMatricesforGivenIntervals...");
-					GenerationofSequencesandMatricesforGivenIntervals.main(allArguments);
+					GenerationofSequencesandMatricesforGivenIntervals.main( args);
 					getMainView().setCurrentProcessInfo( "RSATMatrixScanClient...");
-					RSATMatrixScanClient.main(allArguments);
+					RSATMatrixScanClient.main( args);
 				}
 			}
 			
@@ -127,17 +102,5 @@ public class GlanetRunner extends Thread{
 		GlanetRunner.args = new String[args.length];
 		for( int i = 0; i < args.length; i++) 
 			GlanetRunner.args[i] = args[i];
-	}
-	
-	public static String[] getCellLines() {
-		
-		return cellLines;
-	}
-
-	public static void setCellLines( String cellLines[]) {
-		
-		GlanetRunner.cellLines = new String[cellLines.length];
-		for( int i = 0; i < cellLines.length; i++) 
-			GlanetRunner.cellLines[i] = cellLines[i];
 	}
 }
