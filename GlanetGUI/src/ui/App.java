41ab287dc22787a9031bce15b2ca44aee3920f65
canfirtina <cfirtina@ug.bilkent.edu.tr>
2014-10-06 00:13:53 +0300

    command line fix, thread interrupt check removed

--- GlanetGUI/src/ui/App.java
+++ GlanetGUI/src/ui/App.java
@@ -6,7 +6,6 @@ import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
-
 import common.Commons;
 
 /**
@@ -50,21 +49,23 @@ public class App
 	
 	//args contains all arguments including memory arguments, jar files and class paths
 	//argsForGlanetRunner contains only the arguments required in the GUI
-    public static void main( String[] args) {
-    	
+	public static void main(String args[]) {
+    	 
 		//if Command Line
     	//if last argument is --c
     	if( args.length > 0 && args[args.length-1].startsWith( "--c")) {
     		
     		String argsForGlanetRunner[] = new String[Commons.NUMBER_OF_PROGRAM_RUNTIME_ARGUMENTS];
     		
-    		for(int i =0; i<Commons.NUMBER_OF_PROGRAM_RUNTIME_ARGUMENTS; i++){
+    		for(int i = 0; i < Commons.NUMBER_OF_PROGRAM_RUNTIME_ARGUMENTS; i++){
     			argsForGlanetRunner[i] = args[args.length-Commons.NUMBER_OF_PROGRAM_RUNTIME_ARGUMENTS_INCLUDING_COMMAND_LINE_ARGUMENT+i];
     	    }//End of for
     		
-   		
 //    		argsForGlanetRunner[i++] = args[args.length-23];
 //    		argsForGlanetRunner[i++] = args[args.length-2];
+    		
+    		//for( int i = 0; i < argsForGlanetRunner.length; i++)
+    			//System.out.println( argsForGlanetRunner);
     		
     		GlanetRunner.setMainView( null);
     		GlanetRunner.setArgs( argsForGlanetRunner);
@@ -72,6 +73,7 @@ public class App
     		GlanetRunner runner = new GlanetRunner();
     		runner.start();
     	} else if( !GraphicsEnvironment.isHeadless()) {
-    		loadWindow();}
+    		loadWindow();
     	}
+	}
 }
