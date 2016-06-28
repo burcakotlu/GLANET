e1b710284ca281d77f5f950a489cccad5348aa1d
burcakotlu <burcak@ceng.metu.edu.tr>
2016-06-28 11:46:51 +0300

    SegmentTree and IntervalTree classes are added.

---
+++ Glanet/src/trees/IntervalTree.java
@@ -0,0 +1,31 @@
+/**
+ * 
+ */
+package trees;
+
+/**
+ * @author Burçak Otlu
+ * @date Jun 28, 2016
+ * @project Glanet 
+ * 
+ * Implemented from Computational Geometry, Mark de Berg
+ *
+ */
+public class IntervalTree {
+
+	/**
+	 * 
+	 */
+	public IntervalTree() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ Glanet/src/trees/SegmentTree.java
@@ -0,0 +1,31 @@
+/**
+ * 
+ */
+package trees;
+
+/**
+ * @author Burçak Otlu
+ * @date Jun 28, 2016
+ * @project Glanet 
+ * 
+ * Implemented from Computational Geometry, Mark de Berg
+ *
+ */
+public class SegmentTree {
+
+	/**
+	 * 
+	 */
+	public SegmentTree() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
